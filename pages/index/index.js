510d4002884d5a4617933bdea4eee0bb45e62482
jiang <13611266774@163.com>
2018-12-15 23:13:39 +0800

    '首次提交到github'

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+  },
+ 
+  onLoad: function () {
+    
+  },
+  
+})
